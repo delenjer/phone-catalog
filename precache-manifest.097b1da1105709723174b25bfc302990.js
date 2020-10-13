e6519ff7b4c8b5eb928d5f1061121958300bc902
stanislav.mokhyr <stanislav.mokhyr@idapgroup.com>
2020-10-13 18:10:07 +0300

    Updates

---
+++ precache-manifest.097b1da1105709723174b25bfc302990.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c27a8d24a117e2aef89c9bc3ba8eb52",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1be2047b5146e4ec84d2",
+    "url": "./static/css/main.ec0fb7dd.chunk.css"
+  },
+  {
+    "revision": "f532a13ccf609bfca87f",
+    "url": "./static/js/2.426b2d52.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.426b2d52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be2047b5146e4ec84d2",
+    "url": "./static/js/main.01102bb1.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  },
+  {
+    "revision": "26fc124bfc847d7769460099be08af67",
+    "url": "./static/media/Mont-SemiBold.26fc124b.woff"
+  },
+  {
+    "revision": "68b261cb93089812892941151a21bb65",
+    "url": "./static/media/Mont-SemiBold.68b261cb.eot"
+  },
+  {
+    "revision": "f22588c7bca8654d91410a001a67bc87",
+    "url": "./static/media/Mont-SemiBold.f22588c7.ttf"
+  }
+]);
